a9d98fe8f6e6a006c456144140d636037b2d1954
serenade <makan1869@gmail.com>
2018-08-04 23:16:09 +0430

    Adding Activity Panel

--- src/main/java/ir/serenade/minerva/PublishersPanelApplication.java
+++ src/main/java/ir/serenade/minerva/PublishersPanelApplication.java
@@ -1,10 +1,16 @@
 package ir.serenade.minerva;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class PublishersPanelApplication {
+
+	private static final Logger log = LoggerFactory.getLogger(PublishersPanelApplication.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(PublishersPanelApplication.class, args);
--- src/main/java/ir/serenade/minerva/view/MainView.java
+++ src/main/java/ir/serenade/minerva/view/MainView.java
@@ -1,15 +1,57 @@
 package ir.serenade.minerva.view;
 
-
 import com.vaadin.flow.component.button.Button;
-import com.vaadin.flow.component.notification.Notification;
+import com.vaadin.flow.component.grid.Grid;
+import com.vaadin.flow.component.icon.VaadinIcon;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.textfield.TextField;
+import com.vaadin.flow.data.value.ValueChangeMode;
 import com.vaadin.flow.router.Route;
+import ir.serenade.minerva.domain.Activity;
+import ir.serenade.minerva.repository.ActivityRepository;
 
 @Route
 public class MainView extends VerticalLayout {
 
-    public MainView() {
-        add(new Button("Click me", e -> Notification.show("Hello Spring+Vaadin user!")));
+    private final ActivityRepository repo;
+
+    final Grid<Activity> grid;
+
+    final TextField filter;
+
+    private final Button addNewBtn;
+
+    public MainView(ActivityRepository repo) {
+        this.repo = repo;
+        this.grid = new Grid<>(Activity.class);
+        this.filter = new TextField();
+        this.addNewBtn = new Button("New customer", VaadinIcon.PLUS.create());
+
+        // build layout
+        HorizontalLayout actions = new HorizontalLayout(filter, addNewBtn);
+        add(actions, grid);
+
+        grid.setHeight("300px");
+        grid.setColumns("id", "action", "name", "album", "publisher", "aggregator", "dateCreated");
+        grid.getColumnByKey("id").setWidth("50px").setFlexGrow(0);
+
+        filter.setPlaceholder("Filter by last name");
+
+        // Hook logic to components
+
+        // Replace listing with filtered content when user changes filter
+        filter.setValueChangeMode(ValueChangeMode.EAGER);
+        filter.addValueChangeListener(e -> listCustomers(e.getValue()));
+
+        // Initialize listing
+        listCustomers(null);
     }
+
+    // tag::listCustomers[]
+    void listCustomers(String filterText) {
+        grid.setItems(repo.findAll());
+    }
+    // end::listCustomers[]
+
 }
